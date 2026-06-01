56108bd537228f1acd2a6e3122aa8122b41c6d1e
quorentinnn <quorentinrobichon2@gmail.com>
2026-06-01 12:16:18 +0200

    Delete vite-gourmand-ecf1-main directory

--- vite-gourmand-ecf1-main/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
-// Détruire toutes les variables de session
-$_SESSION = array();
-// Détruire la session
-session_destroy();
-// Rediriger vers la page de connexion
-header('Location: ../public/connexion.php');
-exit;
--- vite-gourmand-ecf1-main/test-mongodb.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Vérifier si l'extension MongoDB est chargée
-if (extension_loaded('mongodb')) {
-    echo "✅ Extension MongoDB est installée !";
-} else {
-    echo "❌ Extension MongoDB n'est PAS installée !";
-}
-?>
